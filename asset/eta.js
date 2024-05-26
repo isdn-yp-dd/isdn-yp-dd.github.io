03af73ae4090d0f00a59a173b80df36131f7143a
tomtom <hwtamad@connect.ust.hk>
2024-05-26 21:33:52 +0800

    update eta every 20s

--- asset/eta.js
+++ asset/eta.js
@@ -1,10 +1,15 @@
 const eta = document.querySelectorAll('div.eta');
 const ppl = document.querySelectorAll('div.ppl');
 
-fetch('https://data.etagmb.gov.hk/eta/route-stop/2004791/1/7')
-  .then(response => response.json())
-  .then(data => {
-    for (let i = 0; i < 3; i++) {
-      eta[i].innerText = data['data']['eta'] [i]['diff'];
-    }
-  });
+function updateETA() {
+  fetch('https://data.etagmb.gov.hk/eta/route-stop/2004791/1/7')
+    .then(response => response.json())
+    .then(data => {
+      for (let i = 0; i < 3; i++) {
+        eta[i].innerText = data['data']['eta'] [i]['diff'];
+      }
+    });
+}
+
+updateETA();
+setInterval(updateETA, 20000);

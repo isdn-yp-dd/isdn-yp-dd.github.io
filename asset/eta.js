3528bebe348c60e3505033c1aaca7a816c5226f4
tomtom <hwtamad@connect.ust.hk>
2024-05-26 21:20:15 +0800

    can show eta once loaded

---
+++ asset/eta.js
@@ -0,0 +1,10 @@
+const eta = document.querySelectorAll('div.eta');
+const ppl = document.querySelectorAll('div.ppl');
+
+fetch('https://data.etagmb.gov.hk/eta/route-stop/2004791/1/7')
+  .then(response => response.json())
+  .then(data => {
+    for (let i = 0; i < 3; i++) {
+      eta[i].innerText = data['data']['eta'] [i]['diff'];
+    }
+  });

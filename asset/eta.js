9a34513c1b790f05ad8c95b7b1459b9d233273e5
tomtom <hwtamad@connect.ust.hk>
2024-05-27 16:51:00 +0800

    should be fine

--- asset/eta.js
+++ asset/eta.js
@@ -1,9 +1,37 @@
+// const
 const eta = document.querySelectorAll('div.eta');
 const ppl = document.querySelectorAll('div.ppl');
+const bus = document.querySelectorAll('div.bus');
+const magic_num = [8, 7, 5, 4, 3, 0];
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
+function showBus(i) {
+  let ETA = eta[i].innerText;
+  if ((ETA == '---') || (parseInt(ETA) > 9)) {
+    bus[i].style.display = 'none';
+  }
+  else {
+    for (let j = 0; j < magic_num.length; j++) {
+      if (parseInt(ETA) > magic_num[j]) {
+        bus[i].style.display = 'block';
+        bus[i].style.top = (715 + 105 * j) + 'px';
+        let value = 19 - parseInt(ppl[i].innerText) + getRandomInt(-1,2);
+        bus[i].innerText = value + '/19';
+        if (value > 13) {
+          bus[i].style.backgroundImage = "url('/asset/bus-2.png')";
+        }
+        else {
+          bus[i].style.backgroundImage = "url('/asset/bus-1.png')";
+        }
+        break;
+      }
+    }
+  }
+}
+
 
 function updateETA() {
   fetch('https://data.etagmb.gov.hk/eta/route-stop/2004791/1/7')
@@ -16,7 +44,7 @@ function updateETA() {
           eta[i].innerText = '---';
           ppl[i].innerText = '0';
         }
-        else if (oldETA != newETA['diff']) {  // eta changed
+        else if ((oldETA == 0) || (oldETA != newETA['diff'])) {  // eta changed
           let value = newETA['diff'];
           if (value < 0) {
             value = 0;
@@ -38,11 +66,13 @@ function updateETA() {
                 break;  // no change
             }
           }
+          showBus(i);
         }
       }
     });
 }
 
+// main
 updateETA();
 ppl.forEach(p => p.innerText = getRandomInt(5, 8));  // init
 setInterval(updateETA, 20000);
